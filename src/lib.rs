101fac5afa0a60e3063f9c47d2460b035503689f
DrEden33773 <edwardwang33773@gmail.com>
2024-06-13 21:04:00 +0800

    generated entities

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub mod entities;
+
+use dotenvy::dotenv;
+use sea_orm::*;
+use std::env;
+
+pub async fn establish_connection() -> Result<DatabaseConnection, DbErr> {
+  dotenv().ok();
+  let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set");
+  let db = Database::connect(&db_url).await?;
+  Ok(db)
+}
